f95d07c35c3b2e3775ef43f7a1a7298f81b22524
Mazedan <daniel05santanabispo@gmail.com>
2022-02-15 16:39:27 -0300

    PrimeiroDeploy

--- php/menu.php
+++ php/menu.php
@@ -4,7 +4,7 @@
         <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                    <i class="bi bi-journal-richtext" style="font-size: 2.5rem;"></i>
+                <i class="bi bi-journal-richtext" style="font-size: 2.5rem;"></i>
               </a>
 
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0"> 
