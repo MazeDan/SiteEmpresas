0ed655f25c09316eee0aba9cf6713e1b2711a6a4
Mazedan <daniel05santanabispo@gmail.com>
2022-02-16 13:52:53 -0300

    Leitor Finalizado

--- php/menu.php
+++ php/menu.php
@@ -1,5 +1,5 @@
 <div class="mb-4"></div>
-<div class="container menu">
+<div class="container">
     <header class="p-2 bg-dark text-white rounded">
         <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
@@ -8,7 +8,7 @@
               </a>
 
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0"> 
-            <li><a href="#" class="nav-link px-3 text-white"><i class="bi bi-qr-code-scan"></i> Leitor</a></li>
+            <li><a href="../arquivos/scan.php" class="nav-link px-3 text-white"><i class="bi bi-qr-code-scan"></i> Leitor</a></li>
             <li><a href="#" class="nav-link px-3 text-white"><i class="bi bi-person-circle"></i> Perfil</a></li>
             <li><a href="#" class="nav-link px-3 text-white"><i class="bi bi-mailbox2"></i> FAQs</a></li>
             <li><a href="#" class="nav-link px-3 text-white"><i class="bi bi-pc-display-horizontal"></i> Sobre</a></li>
---
+++ scan.php
@@ -0,0 +1,35 @@
+<?php 
+    include_once('php/topo.php');
+    include_once('php/menu.php');
+?>
+
+<script src="js/html5-qrcode.min.js"></script>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div id="reader"></div>
+    </div>
+
+    <div class="col">
+      <h4>SCAN RESULT</h4>
+      <div id="result">Result Here</div>
+    </div>
+
+  </div>
+</div>
+<script type="text/javascript">
+function onScanSuccess(qrCodeMessage) {
+    window.location.href = 'http://localhost:8080/arquivos/receber.php?qrcode='+qrCodeMessage;
+    document.getElementById('result').innerHTML = '<span class="result">'+qrCodeMessage+'</span>';
+}
+function onScanError(errorMessage) {
+  //handle scan error
+}
+var html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess, onScanError);
+</script>
+<?php 
+    include_once('rodape.php');
+?>
